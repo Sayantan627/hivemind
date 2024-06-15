093c826f37eb5a2eb874e96c177b48db5c49dc2d
Sayantan_627 <27sayantan.pal@gmail.com>
2024-06-15 12:21:24 +0530

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="pt-12 container max-w-7xl mx-auto h-full">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 py-2 z-[10]">
+      <div className="container h-full max-w-7xl flex mx-auto justify-between items-center gap-2">
+        {/* logo */}
+        <Link href="/" className="flex gap-2 items-center">
+          {/* Logo image */}
+          <p className="text-sm text-zinc-700 font-medium">HiveMind</p>
+        </Link>
+
+        {/* Search bar */}
+
+        <Link href="/sign-in" className={buttonVariants()}>
+          Sign In
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
