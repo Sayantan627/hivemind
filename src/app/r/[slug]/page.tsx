a1521701efe799bf41be5073a0007e2c517af52d
Sayantan_627 <27sayantan.pal@gmail.com>
2024-06-20 13:39:38 +0530

    subreddit details page

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,11 +1,42 @@
+import MiniCreatePost from "@/components/MiniCreatePost";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { notFound } from "next/navigation";
+
 interface PageProps {
   params: {
     slug: string;
   };
 }
 
-const page = ({ params }: PageProps) => {
+const page = async ({ params }: PageProps) => {
   const { slug } = params;
-  return <div>page</div>;
+  const session = await getAuthSession();
+  const subreddit = await prisma.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          subreddit: true,
+          comments: true,
+          votes: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+      },
+    },
+  });
+
+  if (!subreddit) return notFound();
+  return (
+    <>
+      <h1 className="h-14 text-3xl font-bold md:text-4xl">
+        r/{subreddit.name}
+      </h1>
+      <MiniCreatePost session={session} />
+      {/* TODO: Show posts in user feed */}
+    </>
+  );
 };
 export default page;
---
+++ src/components/MiniCreatePost.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Session } from "next-auth";
+import { usePathname, useRouter } from "next/navigation";
+import { FC } from "react";
+import UserAvatar from "./UserAvatar";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { ImageIcon, Link2 } from "lucide-react";
+
+interface MiniCreatePostProps {
+  session: Session | null;
+}
+
+const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <li className="overflow-hidden rounded-md bg-white shadow">
+      <div className="sm: flex h-full justify-end gap-6 px-6 py-4">
+        <div className="relative">
+          <UserAvatar
+            user={{
+              name: session?.user.name || null,
+              image: session?.user.image || null,
+            }}
+          />
+          <span className="absolute bottom-0 right-0 h-3 w-3 rounded-full bg-green-500 outline outline-2 outline-white" />
+        </div>
+        <Input
+          placeholder="Create post"
+          readOnly
+          onClick={() => router.push(pathname + "/submit")}
+        />
+        <Button
+          variant="ghost"
+          onClick={() => router.push(pathname + "/submit")}
+        >
+          <ImageIcon className="text-zinc-600" />
+        </Button>
+        <Button
+          variant="ghost"
+          onClick={() => router.push(pathname + "/submit")}
+        >
+          <Link2 className="text-zinc-600" />
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default MiniCreatePost;
