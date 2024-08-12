bcb1178b15a9d4a300e42c301aac313e00f4f1aa
Sayantan_627 <27sayantan.pal@gmail.com>
2024-08-13 01:30:50 +0530

    fix error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export const middleware = async (req: NextRequest) => {
   const token = await getToken({ req });
